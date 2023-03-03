666f0c262eb7abca7c83e4b61453d31ec91b36cd
朱勇龙 <379368435@qq.com>
2023-03-03 23:30:04 +0800

    v1.2.23 NTabs中使用NLink

--- components/NTabs.vue
+++ components/NTabs.vue
@@ -14,8 +14,13 @@
 						: 'text-indigo-500 border-indigo-500'
 				]"
 			>
-				<a v-if="tab.to" :href="tab.to" :target="tab.target" class="block p-4">{{ tab.label }}</a>
-				<a v-else-if="queryName" :href="`?${queryName}=${tab.value}`" @click.prevent="onChange(tab, key)" class="block p-4">
+				<NLink v-if="tab.to" :to="tab.to" :target="tab.target" class="block p-4">{{ tab.label }}</NLink>
+				<a
+					v-else-if="queryName"
+					:href="`?${queryName}=${tab.value}`"
+					@click.prevent="onChange(tab, key)"
+					class="block p-4"
+				>
 					{{ tab.label }}
 				</a>
 				<div v-else class="p-4 cursor-pointer" @click.stop="onChange(tab, key)">{{ tab.label }}</div>
