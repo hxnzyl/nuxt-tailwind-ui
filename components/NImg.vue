351e0e838453c191033cc33ae81b97cc1cdd8474
zhuyonglong <110017640+hxnzyl@users.noreply.github.com>
2023-07-05 18:15:18 +0800

    1.0.47 图片支持跨域

--- components/NImg.vue
+++ components/NImg.vue
@@ -1,16 +1,16 @@
 <template>
 	<a v-if="to" :href="to" :target="target" class="n-img overflow-hidden rounded-md">
-		<img v-if="lazy" :lazy-src="src" :class="nativeClass" />
-		<img v-else :src="src" :class="nativeClass" />
+		<img v-if="lazy" :lazy-src="src" :class="nativeClass" v-bind="commonAttrs" />
+		<img v-else :src="src" :class="nativeClass" v-bind="commonAttrs" />
 		<slot></slot>
 	</a>
 	<div v-else class="n-img overflow-hidden rounded-md" :class="{ 'cursor-pointer': preview }" @click="onClick">
-		<img v-if="lazy" :lazy-src="src" :class="nativeClass" />
-		<img v-else :src="src" :class="nativeClass" />
+		<img v-if="lazy" :lazy-src="src" :class="nativeClass" v-bind="commonAttrs" />
+		<img v-else :src="src" :class="nativeClass" v-bind="commonAttrs" />
 		<slot></slot>
 		<NModal v-model="previewModal" :show-header="false" :show-footer="false">
 			<template #body>
-				<img :src="previewSrc" />
+				<img :src="previewSrc" v-bind="commonAttrs" />
 			</template>
 		</NModal>
 	</div>
@@ -33,6 +33,13 @@ export default {
 	computed: {
 		nativeClass() {
 			return [this.preview ? '' : tailwindui.hoverScale(this.scale), this.imgClass]
+		},
+		commonAttrs() {
+			var attrs = {}
+			//crossorigin
+			if (!!this.src && /^http/.test(this.src) && !new RegExp(`^${location.protocol}//${location.host}`).test(this.src))
+				attrs.crossorigin = ''
+			return attrs
 		}
 	},
 	data() {
